9778f6126d9a55edc2a634317e284f327cefb547
shinichi <shinichi@hotmail.nl>
2020-10-06 12:11:19 +0200

    added some TCP logic

--- CSharpEindopdracht/Client/Client.cs
+++ CSharpEindopdracht/Client/Client.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Server
 {
-    class Client
+    internal class Client
     {
+        private TcpClient client;
+        private NetworkStream stream;
+        private byte[] buffer = new byte[1024];
+        private byte[] totalBuffer = new byte[1024];
+        private int totalBufferReceived = 0;
         private string username;
         private int clientID;
 
-        public Client()
+        public Client(TcpClient tcpClient, int clientID)
         {
-
+            this.clientID = clientID;
         }
+
+        private void OnRead(IAsyncResult ar)
+        {
+            int receivedBytes = this.stream.EndRead(ar);
+
+            if (totalBufferReceived + receivedBytes > buffer.Length)
+            {
+                throw new OutOfMemoryException("buffer too small");
+            }
+            Array.Copy(buffer, 0, totalBuffer, totalBufferReceived, receivedBytes);
+            totalBufferReceived += receivedBytes;
+
+            int expectedMessageLength = BitConverter.ToInt32(totalBuffer, 0);
+            while (totalBufferReceived >= expectedMessageLength)
+            {
+                //volledig packet binnen
+                byte[] messageBytes = new byte[expectedMessageLength];
+                Array.Copy(totalBuffer, 0, messageBytes, 0, expectedMessageLength);
+
+
+                //byte[] payloadbytes = new byte[BitConverter.ToInt32(messageBytes, 0) - 5];
+
+                //Array.Copy(messageBytes, 5, payloadbytes, 0, payloadbytes.Length);
+
+            }
+
+            this.stream.BeginRead(this.buffer, 0, this.buffer.Length, new AsyncCallback(OnRead), null);
+        }
+
+
     }
 }
