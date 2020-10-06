aeb25966ebb6305b5e22273f5c630d7e6efb445c
shinichi <shinichi@hotmail.nl>
2020-10-06 10:56:42 +0200

    added shared projects

---
+++ CSharpEindopdracht/SharedSkribbl/Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedSkribbl
+{
+    struct Line
+    {
+        public int BeginX { get; set }
+        public int BeginY { get; set }
+        public int EndX { get; set }
+        public int EndY { get; set }
+        public int StrokeWidth { get; set; }
+    }
+}
