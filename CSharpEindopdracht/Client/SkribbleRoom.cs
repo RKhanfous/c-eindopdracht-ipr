450e6c7e1bbd59c27a2a09135b33a200a9dec88c
shinichi <shinichi@hotmail.nl>
2020-10-13 14:15:56 +0200

    added constructors

--- CSharpEindopdracht/Client/SkribbleRoom.cs
+++ CSharpEindopdracht/Client/SkribbleRoom.cs
@@ -12,21 +12,39 @@ namespace Server
     {
         #region private Members
 
-        private string RoomCode;
+        private NetworkHandler networkHandler;
+        private string roomCode;
         private List<Player> players;
         private int currentRound;
-        private int maxNumRounds;
+        private int numRounds;
         private List<string> words;
         private List<string> usedWords;
         private string currentWord;
+        private Player currentPlayer;
+        private List<Player> drawingPlayers;
+        private bool running;
 
         #endregion
 
         #region constructors
 
-        public SkribbleRoom()
+        public SkribbleRoom(NetworkHandler networkHandler)
         {
+
+        }
+
+        public SkribbleRoom(NetworkHandler networkHandler, string roomCode)
+        {
+
+        }
+
+        public SkribbleRoom(NetworkHandler networkHandler, string roomCode, int numberOfRounds)
+        {
+            this.networkHandler = networkHandler;
+            this.roomCode = roomCode;
             this.players = new List<Player>();
+            this.currentRound = 0;
+            this.numRounds = numberOfRounds;
         }
 
         #endregion
@@ -37,7 +55,30 @@ namespace Server
         /// <param name="objectState"></param>
         public void Run(object objectState)
         {
+            while (running)
+            {
 
+            }
         }
+
+        #region Getters and setters
+
+        public void AddPlayer(Player player)
+        {
+            lock (this.players)
+            {
+                this.players.Add(player);
+            }
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            lock (this.players)
+            {
+                this.players.Remove(player);
+            }
+        }
+
+        #endregion
     }
 }
