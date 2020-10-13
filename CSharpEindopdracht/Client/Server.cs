5ce1058617373497e6754536e0c23add1ef3da52
shinichi <shinichi@hotmail.nl>
2020-10-13 14:02:58 +0200

    start adding skribbl

--- CSharpEindopdracht/Client/Server.cs
+++ CSharpEindopdracht/Client/Server.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Server
 {
     class Server
     {
+        private NetworkHandler networkHandler;
+
+        public Server()
+        {
+        }
     }
 }
---
+++ CSharpEindopdracht/Client/SkribbleRoom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    /// <summary>
+    /// skribbl room
+    /// 
+    /// </summary>
+    class SkribbleRoom
+    {
+        #region private Members
+
+        private string RoomCode;
+        private List<Player> players;
+        private int currentRound;
+        private int maxNumRounds;
+        private List<string> words;
+        private List<string> usedWords;
+        private string currentWord;
+
+        #endregion
+
+        #region constructors
+
+        public SkribbleRoom()
+        {
+            this.players = new List<Player>();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// main loop for the room
+        /// </summary>
+        /// <param name="objectState"></param>
+        public void Run(object objectState)
+        {
+
+        }
+    }
+}
