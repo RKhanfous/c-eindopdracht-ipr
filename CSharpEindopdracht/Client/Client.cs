634315d69f5abc6b297b8a5cfb837692aff1aab8
Redouan Khanfous <r.khanfous@student.avans.nl>
2020-10-17 13:07:02 +0200

    [ADDITION] added some stuff to networkhandler and server

--- CSharpEindopdracht/Client/Client.cs
+++ CSharpEindopdracht/Client/Client.cs
@@ -1,9 +1,4 @@
-﻿using Networking;
-using SharedNetworking.Utils;
-using System;
-using System.Collections.Generic;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net.Sockets;
 
 namespace Server
 {
