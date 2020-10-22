e15ff55f8e6700114cf577e7dafe7d92eddce8b6
shinichi <shinichi@hotmail.nl>
2020-10-22 11:53:28 +0200

    packages geïnstalleerd

--- CSharpEindopdracht/WpfClient/ViewModels/GameViewModel.cs
+++ CSharpEindopdracht/WpfClient/ViewModels/GameViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Text;
+using System.Windows.Input;
 using WpfClient.Models;
 using WpfClient.Utils;
 
@@ -16,9 +19,16 @@ namespace WpfClient.ViewModels
         public ObservableCollection<Player> Players { get; set; }
         public ObservableCollection<string> Chat { get; set; }
 
+        public ICommand MouseDownCommand { get; set; }
+        public ICommand MouseMoveCommand { get; set; }
+
         public GameViewModel(MainViewModel mainViewModel, string username)
         {
             this.mainViewModel = mainViewModel;
+
+            this.MouseDownCommand = new RelayCommand<object>((param) => { Debug.WriteLine("mouseDown " + param.GetType()); });
+
+            this.MouseMoveCommand = new RelayCommand<object>((param) => { Debug.WriteLine("mouseMove " + param.GetType()); });
         }
     }
 }
