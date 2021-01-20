33e2bd1129c35c48504e7ffedf55495d4ca3b5b0
Redouan Khanfous <r.khanfous@student.avans.nl>
2021-01-20 19:49:56 +0100

    [FIX] added append true to streamwriter dont know if this fixed it.

--- CSharpEindopdracht/Client/Logger.cs
+++ CSharpEindopdracht/Client/Logger.cs
@@ -17,19 +17,20 @@ namespace Server
         {
             this.logPath = logPathApp;
             this.logName = "SystemLog" + StringBuilderTime() + ".txt";
-            this.outputFile = new StreamWriter(Path.Combine(logPath, logName));
+            this.outputFile = new StreamWriter(Path.Combine(logPath, logName), true);
         }
 
         public void logException(String exception)
         {
-            using (outputFile) {
+            using (this.outputFile) {
+
                 this.outputFile.WriteLine("[" + DateTime.Now + "] this exception has occured: " + exception + "!");
             }
         }
 
         public void logServer()
         {
-            using (outputFile)
+            using (this.outputFile)
             {
                 this.outputFile.WriteLine("[" + DateTime.Now + "] server started!");
             }
@@ -37,7 +38,7 @@ namespace Server
 
         public void logStartGame(Player player)
         {
-            using (outputFile)
+            using (this.outputFile)
             {
                 this.outputFile.WriteLine("[" + DateTime.Now + "] player:" + player.username + "with clientID:" + player.clientID + " started a game.");
             }
@@ -45,7 +46,7 @@ namespace Server
 
         public void logConnectClient(int clientID)
         {
-            using (outputFile)
+            using (this.outputFile)
             {
                 this.outputFile.WriteLine("[" + DateTime.Now + "]" + clientID + " connected to the server!");
             }
@@ -53,7 +54,7 @@ namespace Server
 
         public void logDisconnectClient(Client client)
         {
-            using (outputFile)
+            using (this.outputFile)
             {
                 this.outputFile.WriteLine("[" + DateTime.Now + "]" + client.ClientId + " left the server!");
             }
