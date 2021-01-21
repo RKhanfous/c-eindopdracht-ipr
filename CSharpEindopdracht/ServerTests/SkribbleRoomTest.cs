ade8dcdbdfe81e800082a3c2b233fb0c4b22542a
shinichi <shinichi@hotmail.nl>
2021-01-21 19:36:31 +0100

    [ADD] added testLogger

---
+++ CSharpEindopdracht/ServerTests/SkribbleRoomTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerTests
+{
+    [TestClass]
+    class SkribbleRoomTest
+    {
+        [TestMethod]
+        public void test1()
+        {
+
+        }
+    }
+}
